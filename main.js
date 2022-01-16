1d5484ab15b3231605e5f87d0602085fefb98cdd
l31tu <70357291+l31tu@users.noreply.github.com>
2022-01-16 20:41:05 +1100

    Update svg and javascript for information toggle

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-$('.menu-toggle').on('click', function () {
-  
-  $('.information').toggleClass('open')
-  
-  return false
-  
+const toggleInfoTag = document.querySelector("a.toggle-info")
+const exitTag = document.querySelector("div.toggle-info")
+const infoTag = document.querySelector(".information")
+
+toggleInfoTag.addEventListener("click", () => {
+  infoTag.classList.toggle("open")
+})
+
+exitTag.addEventListener("click", () => {
+  infoTag.classList.remove("open")
 })
