40d554fb03f15abf5722561cc88688a4e65e63ef
l31tu <70357291+l31tu@users.noreply.github.com>
2022-07-13 23:14:44 +1000

    Add test 3 for TG app

---
+++ afaafakasi/lomiga-lua-test-3.js
@@ -0,0 +1,10 @@
+// https://stackoverflow.com/questions/30267973/how-to-repeat-div-element-n-times-in-html
+
+function multiplyNode(node, count, deep) {
+    for (var i = 0, copy; i < count - 1; i++) {
+        copy = node.cloneNode(deep);
+        node.parentNode.insertBefore(copy, node);
+    }
+}
+
+multiplyNode(document.querySelector('div'), 2000, true);
